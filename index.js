f55407f6cafe05ca2c31e9260a853dc295753c25
caiphus <caiphus@thedigitalacademy.co.za>
2022-09-04 05:44:50 +0200

    notes added

--- index.js
+++ index.js
@@ -2,19 +2,24 @@ const express = require('express');
 const xrpl = require("xrpl"); //importing xrp Ledger
 const dotenv = require('dotenv');
 const app = express();
+const cors = require('cors');
 const bodyParser = require('body-parser');
 
 // support parsing of application/json type post data
 app.use(bodyParser.json());
 
+
 //support parsing of application/x-www-form-urlencoded post data
 app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(cors());
 
 app.use(express.urlencoded({
     extended: true
   }))
 
 let url = process.env.TestNet || "wss://s.altnet.rippletest.net:51233" ;
+const PORT = process.env.PORT || 3000 ;
 
 // const wallet = new xrpl.Wallet(process.env.PUBLICKEY1, process.env.PRIVATEKEY1);
 const client = new xrpl.Client(url);
@@ -30,7 +35,6 @@ app.use('/sign_blob', async (req, res)=>{
         })
 
 })
-
 
 app.use('/get_Wallet_Using_Seed' ,async (req, res) => {
 
@@ -56,16 +60,14 @@ app.use('/ledger_events',async (req,res)=>{
                  })
 })
 
-app.use('/signing_Transaction', async (req, res) => {
+app.post('/signing_Transaction', async (req, res) => {
 
     const { prepared ,publicKey ,privateKey } = req.body;
+    // console.log(prepared ,publicKey ,privateKey)
     const wallet = new xrpl.Wallet(publicKey, privateKey);
-    // Sign prepared instructions ------------------------------------------------
-     const signed = wallet.sign(prepared)
-     console.log("Identifying hash:", signed.hash)
-     console.log("Signed blob:", signed.tx_blob)
-   
-    res.send(signed.tx_blob.json())
+    //Sign prepared instructions ------------------------------------------------
+    const signed_blob = wallet.sign(prepared);  
+    res.send(signed_blob)
 
 })
 
@@ -85,25 +87,26 @@ app.use('/check_Transaction_Status', async (req, res) => {
 
 app.post('/send', async (req, res) => {
     const { sender_Wallet_Address,receiver_Wallet_Address,xrp_Amount_To_Drop } = req.body;
+    // console.log('sender_Wallet_Address :',sender_Wallet_Address , "receiver_Wallet_Address :",receiver_Wallet_Address , "  xrp_Amount_To_Drop :",xrp_Amount_To_Drop )
     await client.autofill({
            "TransactionType": "Payment",
-           "Account": sender_Wallet_Address,
-           "Amount": xrpl.xrpToDrops(xrp_Amount_To_Drop),
-           "Destination": receiver_Wallet_Address
+           "Account": `${sender_Wallet_Address}`,
+           "Amount": xrpl.xrpToDrops(`${xrp_Amount_To_Drop}`),
+           "Destination": `${receiver_Wallet_Address}`
              }).then((response) => {
                 res.send(response)
-             })
+             }).catch((err) => {res.send(err)})
 })
 
-app.use('/get_funded_wallet_info', async (req, res) => {
-   const {classic_Address} = req.body;
+app.post('/get_funded_wallet_info', async (req, res) => {
+   const { classic_Address } = req.body;
          await client.request({
             "command": "account_info",
             "account": classic_Address,
             "ledger_index": "validated"
           })
           .then((wallet_Info) => { 
-            res.seed(wallet_Info.json());
+            res.send(wallet_Info);
         })
        
     
@@ -118,14 +121,18 @@ app.use('/genFundWallet', async (req, res) => {
 //  If you just want to generate keys, you can create a new Wallet instance like this:
 app.use('/new_Wallet_Instance', async (req, res) => {
     const test_wallet = xrpl.Wallet.generate();
-    res.send(test_wallet.toJSON());
+    res.send(test_wallet);
 })
 
 app.use('/disconnect', async (req, res) => {
-    client.disconnect().
-    then((res) => { 
-        console.log("XRP Ledger is Disconnected Successfully ..!");
-        }).catch(err => console.log(err))
+    let isConnected;
+    client.disconnect()
+    .then(() => { 
+        isConnected = false ;
+        res.send({connected:isConnected});
+        }).catch(
+            err => console.log(err)
+        )
 })
 
 app.use('/connect', async (req, res) => {
@@ -147,6 +154,6 @@ app.get('/', async (req, res) => {
    res.send("XRP Ledger is ready to use...!")
  })
 
-app.listen(3000,() => {
-    console.log('listening on port 3000')
+app.listen(PORT,() => {
+    console.log('Running on:' , "http//:localhost:",PORT)
 })
